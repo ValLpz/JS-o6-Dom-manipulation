e587fe8671722ca1fe23c6cc6f24a374316e98f7
ValLpz <val.lpz.glz@gmail.com>
2024-08-08 04:19:36 -0600

    Add files via upload
    Código refactorizado

--- Cards.js
+++ Cards.js
@@ -1,40 +1,65 @@
 const IMAGE_URL = "https://placehold.co/200";
 
-let USER = {
-    id:3,
-    username: "User Name",
-    description: "Me gustan los conejos",
-    age: "26",
-    fav_music: {
-        bands: ["Rammstein", "  Interpol", " TOOL", "  Black Sabbath."],
-    },
+function creatUSERn(user) {
+   //card
+    const card= document.createElement("div")
+    card.classList.add("card-container");
+    //img
+    const cardImgContainer= document.createElement("div");
+    cardImgContainer.classList.add("card-img-container");
+
+    const imageElement= document.createElement("img");
+    imageElement.src= IMAGE_URL; 
+    imageElement.alt="User profile photo";
+
+    //title
+    const cardTitleElement= document.createElement("h3");
+    cardTitleElement.classList.add("card-title");
+
+    //info users
+    const cardAgeElement= document.createElement("p");
+    const cardDescElement= document.createElement("p");
+    const cardFavElement= document.createElement("p");
+
+    //poblar
+    imageElement.src = IMAGE_URL;
+    cardTitleElement.textContent = user.username;
+    cardAgeElement.textContent = `Age: ${user.age}`;
+    cardDescElement.textContent = `Description: ${user.description}`;
+    cardFavElement.textContent = `Favorite Bands: ${user.fav_music.bands.join(", ")}`;
+    
+    //renderizar
+    cardImgContainer.appendChild(imageElement);
+    card.append(cardImgContainer, cardTitleElement, cardAgeElement, cardDescElement, cardFavElement);
+    
+    return card;
 };
+    const users = [
+        {
+            id: 1,
+            username: "User 1 ",
+            description: "lorem ipsum",
+            age: "46",
+            fav_music: {
+                bands: [
+                    "Band 1", "Band 2", "Band 3", "Band 4" ]
+            }
+        },
+        {
+            id: 2,
+            username: "User LastName",
+            description: "Lorem lorem lorem",
+            age: "23",
+            fav_music: {
+                bands: [ "Band 1", "Band 2", "Band 3", "Band 4"]
+            }
+        }
+    ];
+    const cardContainer = document.getElementById("card-container");
+    users. forEach(user => {
+        const userScard = creatUSERn(user);
+        cardContainer.appendChild(userScard)
+    });
 
 
-//Selectores
-const card = document.getElementById("card-container");
-const cardImgContainer = document.getElementById("card-img-container");
-
-//Crear elemento img
-
-const imageElement= document.createElement("img");
-imageElement.src = IMAGE_URL;
-imageElement.alt= "User profile photo";
-
-//Creamos elemento titulo -> card
-const cardTitleElement= document.createElement("h3");
-cardTitleElement.classList.add("card-title");
-const cardAgeElement = document.createElement("p");
-const cardDescElement= document.createElement("p");
-const cardFavElement= document.createElement("p");
-
-//Poblar
-cardTitleElement.textContent= USER.username;
-cardAgeElement.textContent= USER.age;
-cardDescElement.textContent= USER.description;
-cardFavElement.textContent= USER.fav_music.bands;
-
-//Renderizar
-cardImgContainer.appendChild(imageElement);
-card.append(cardTitleElement, cardAgeElement, cardDescElement, cardFavElement);
 
--- Dom.js
+++ Dom.js
@@ -1,14 +1,21 @@
-const p1= document.getElementById("p1");
-const h2List= document.getElementsByClassName("subtitle");
+/*const p1= document.getElementById("p1");
+//const h2List= document.getElementsByClassName("subtitle");
 const textInput= document.getElementById("username-input");
 
 const element1= document.querySelector("#p1");
 const element2= document.querySelector(".subtitle");
 
-console.log(p1);
-console.log(h2List);
-console.log(textInput);
+//console.log(p1);
+//console.log(h2List);
+//console.log(textInput);
 
 textInput.addEventListener("input", (e) => {
     p1.textContent = e.target.value;
-});
+});*/
+
+/*const creatUSERn = document.getElementById("card-container");
+
+userS.forEach(user => {
+    const userCard = creatUSERn(users);
+    cardContainer.appendChild(userCard);
+});*/
